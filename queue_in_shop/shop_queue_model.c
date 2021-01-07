bc7f24c0b40601ca16c26e18281077eaca66d07f
Artyom Erofeev <34557625+ErofeevAA@users.noreply.github.com>
2021-01-08 02:09:02 +1000

    Add files via upload

--- queue_in_shop/shop_queue_model.c
+++ queue_in_shop/shop_queue_model.c
@@ -1,34 +1,46 @@
 #include <ncurses.h>
 #include <stdlib.h>
-//#include <stdio.h>
 #include "shop_queue_model.h"
 #include "draw.h"
+#include "operation_shop_struct.h"
+#include "coords.h"
 
 static int max_customer_time;
-static int max_cashier_queue;
+static int max_cashier_queue_length;
 static int max_cashiers;
 static int max_next_customers;
 
 static Cashier *g_cashiers = NULL;
 
+static int g_time = 0;
+static int g_served_customers = 0;
+
 void controller();
 void model();
 void init_settings();
+void init_cashiers();
 
 void init_model() {
     init_settings();
-    g_cashiers = malloc(max_cashiers * sizeof(Cashier));
+    init_cashiers();
     initscr();
     noecho();
     curs_set(0);
     keypad(stdscr, true);
     halfdelay(10);
     draw_head();
-
+    draw_all_cashiers(max_cashier_queue_length, max_cashiers, g_cashiers);
+    draw_time(g_time);
+    //draw_next_customers();
+    //draw_sum_customers(0);
+    //draw_sum_working_cashiers();
+    draw_sum_served_customers(g_served_customers);
+    draw_max_queue_cashier_length(max_cashier_queue_length);
     controller();
 }
 
 void destroy_model() {
+    free_all_cashiers_queue(&g_cashiers, max_cashiers);
     free(g_cashiers);
     endwin();
 }
@@ -37,13 +49,14 @@ void controller() {
     const int KEY_ESC = 27;
     int key = getch();
     while (key != KEY_ESC) {
+        g_time++;
         model();
         key = getch();
     }
 }
 
 void model() {
-
+    draw_time(g_time);
 }
 
 #pragma clang diagnostic push
@@ -57,7 +70,7 @@ void init_settings() {
     fscanf(file, "%d", &max_customer_time);
     fscanf(file, "%s", word);
     fscanf(file, "%s", word);
-    fscanf(file, "%d", &max_cashier_queue);
+    fscanf(file, "%d", &max_cashier_queue_length);
     fscanf(file, "%s", word);
     fscanf(file, "%s", word);
     fscanf(file, "%d", &max_cashiers);
@@ -65,5 +78,19 @@ void init_settings() {
     fscanf(file, "%s", word);
     fscanf(file, "%d", &max_next_customers);
     fclose(file);
+    int y_cur = 3 + max_cashier_queue_length + y_cashier;
+    y_time = y_cur++;
+    y_next_customers = y_cur++;
+    y_sum_customers = y_cur++;
+    y_working_cashiers = y_cur++;
+    y_sum_served_customers = y_cur++;
+    y_max_length_queue_cashier = y_cur;
 }
 #pragma clang diagnostic pop
+
+void init_cashiers() {
+    g_cashiers = malloc(max_cashiers * sizeof(Cashier));
+    for (int i = 0; i < max_cashiers; ++i) {
+        g_cashiers[i].queue = NULL;
+    }
+}
